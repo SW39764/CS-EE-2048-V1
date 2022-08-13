3065d14e9f71e81d3827f60f6a86b2cf9793d872
Sam <weisz.sam321@gmail.com>
2022-08-13 17:57:58 +0200

    Initial commit

---
+++ Game.py
@@ -0,0 +1,178 @@
+import random
+from copy import deepcopy
+
+size = 4
+
+def addRandom(n, i = 1):
+    zeroes = False
+    for y in n:
+        for x in y:
+            if x == 0:
+                zeroes = True
+
+    if not zeroes:
+        return n
+
+    for _ in range(i):
+        while True:
+            x = int(random.randint(0,3))
+            y = int(random.randint(0,3))
+            if n[x][y] == 0:
+                n[x][y] = (random.choices([2,4], weights = (4,1)))[0]
+                break
+    return n
+
+def printArr(n):
+    for y in n:
+        print(y)
+    print()
+
+def moveLeft(n):
+    curr = deepcopy(n)
+    for it in range(size):
+        row = curr[it]
+        for _ in range(size-1):
+            for x in range(1, size):
+                if row[x-1] == 0:
+                    row[x-1] = row[x]
+                    row[x] = 0
+
+        for i in range(size - 1):
+            if(row[i] == row[i+1]):
+                row[i] *= 2
+                row[i+1] = 0
+
+        for x in range(1, size):
+            if row[x - 1] == 0:
+                row[x - 1] = row[x]
+                row[x] = 0
+
+        curr[it] = row
+    curr = addRandom(curr, 1)
+    return curr
+
+def moveRight(n):
+    curr = deepcopy(n)
+    for it in range(size):
+        row = curr[it]
+        for _ in range(size - 1):
+            for x in range(size-2, -1, -1):
+                if row[x + 1] == 0:
+                    row[x + 1] = row[x]
+                    row[x] = 0
+
+        for i in range(size - 1, 0, -1):
+            if (row[i] == row[i -1]):
+                row[i] *= 2
+                row[i - 1] = 0
+
+        for x in range(size - 2, -1, -1):
+            if row[x + 1] == 0:
+                row[x + 1] = row[x]
+                row[x] = 0
+
+        curr[it] = row
+    curr = addRandom(curr, 1)
+    return curr
+
+def moveDown(n):
+    curr = deepcopy(n)
+    for it in range(size):
+        col = [x[it] for x in curr]
+
+        # print(f"\n{col}")
+
+        for _ in range(size - 1):
+            for x in range(size-2, -1, -1):
+                if col[x + 1] == 0:
+                    col[x + 1] = col[x]
+                    col[x] = 0
+
+        for i in range(size - 1, 0, -1):
+            if (col[i] == col[i -1]):
+                col[i] *= 2
+                col[i - 1] = 0
+
+        for x in range(size - 2, -1, -1):
+            if col[x + 1] == 0:
+                col[x + 1] = col[x]
+                col[x] = 0
+
+        # print(f"{col}\n")
+
+        for i, val in enumerate(col):
+            curr[i][it] = val
+    curr = addRandom(curr, 1)
+    return curr
+
+def moveUp(n):
+    curr = deepcopy(n)
+    for it in range(size):
+        col = [x[it] for x in curr]
+
+        # print(f"\n{col}")
+
+        for _ in range(size - 1):
+            for x in range(1, size):
+                if col[x - 1] == 0:
+                    col[x - 1] = col[x]
+                    col[x] = 0
+
+        for i in range(size - 1):
+            if (col[i] == col[i + 1]):
+                col[i] *= 2
+                col[i + 1] = 0
+
+        for x in range(1, size):
+            if col[x - 1] == 0:
+                col[x - 1] = col[x]
+                col[x] = 0
+
+        # print(f"{col}\n")
+
+        for i, val in enumerate(col):
+            curr[i][it] = val
+
+    curr = addRandom(curr, 1)
+    return curr
+
+def gameOver(n):
+
+    return n == moveUp(n) and n == moveDown(n) and n == moveLeft(n) and n == moveRight(n)
+
+def getScore(n):
+    score = 0
+    for row in n:
+        for val in row:
+            score += val
+    return score
+
+def getEmpty(n):
+    empty = 0
+    for row in n:
+        for val in row:
+            if val == 0:
+                empty += 1
+    return empty
+
+def getMax(n):
+    max = 0
+    for row in n:
+        for val in row:
+            if val > max:
+                max = val
+    return max
+
+
+
+if(__name__ == "__main__"):
+    arr = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
+
+    arr = addRandom(arr, 12)
+
+    printArr(arr)
+
+    arr = moveUp(arr)
+
+    printArr(arr)
+
---
+++ MCTS.py
@@ -0,0 +1,104 @@
+import random
+from Game import *
+from copy import deepcopy
+
+mainGame = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
+mainGame = addRandom(mainGame, 2)
+
+iter = 15
+
+
+
+def randomMove():
+    pmoves = ["moveLeft", "moveRight", "moveUp", "moveDown"]
+    choice = random.choice(pmoves)
+    return(choice)
+
+
+def runRandom(n):
+    while (not gameOver(n)):
+
+        chosen = randomMove() + "(n)"
+        n = eval(chosen)
+        # print(f"Moved {chosen[4:-3]}")
+        # printArr(n)
+
+    # print("\n\nGame Over\n")
+    # printArr(n)
+    return n
+
+def oneIteration(inputGame):
+    scoreLeft = 0
+    scoreRight = 0
+    scoreUp = 0
+    scoreDown = 0
+
+    emptyLeft = 0
+    emptyRight = 0
+    emptyUp = 0
+    emptyDown = 0
+
+    maxLeft = 0
+    maxRight = 0
+    maxUp = 0
+    maxDown = 0
+
+    for _ in range(iter):
+        #left
+        n = deepcopy(inputGame)
+        n = moveLeft(n)
+        n = runRandom(n)
+        scoreLeft += getScore(n)
+        maxLeft += getMax(n)
+
+        #right
+        n = deepcopy(inputGame)
+        n = moveRight(n)
+        n = runRandom(n)
+        scoreRight += getScore(n)
+        maxRight += getMax(n)
+
+        #up
+        n = deepcopy(inputGame)
+        n = moveUp(n)
+        n = runRandom(n)
+        scoreUp += getScore(n)
+        maxUp += getMax(n)
+
+        #down
+        n = deepcopy(inputGame)
+        n = moveDown(n)
+        n = runRandom(n)
+        scoreDown += getScore(n)
+        maxDown += getMax(n)
+
+    qualityLeft = (scoreLeft / iter)/2 + (maxLeft / iter)
+    qualityRight = (scoreRight / iter)/2 + (maxRight / iter)
+    qualityUp = (scoreUp / iter)/2 + (maxUp / iter)
+    qualityDown = (scoreDown / iter)/2 + (maxDown / iter)
+
+    # print(scoreLeft / iter, scoreRight / iter, scoreUp / iter, scoreDown / iter)
+    # print(maxLeft / iter, maxRight / iter, maxUp / iter, maxDown / iter)
+
+
+    # print(f"Average Left Score: {qualityLeft}")
+    # print(f"Average Right Score: {qualityRight}")
+    # print(f"Average Up Score: {qualityUp}")
+    # print(f"Average Down Score: {qualityDown}")
+
+    if qualityLeft > qualityRight and qualityLeft > qualityUp and qualityLeft > qualityDown:
+        inputGame = moveLeft(inputGame)
+    elif qualityRight > qualityLeft and qualityRight > qualityUp and qualityRight > qualityDown:
+        inputGame = moveRight(inputGame)
+    elif qualityUp > qualityLeft and qualityUp > qualityRight and qualityUp > qualityDown:
+        inputGame = moveUp(inputGame)
+    else:
+        inputGame = moveDown(inputGame)
+    return inputGame
+
+
+if __name__ == '__main__':
+    while (not gameOver(mainGame)):
+        mainGame = oneIteration(mainGame)
+        printArr(mainGame)
+        print(getScore(mainGame))
